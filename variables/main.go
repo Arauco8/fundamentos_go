e4ddc48ecf924a29c7d8afe71c92fc5a5d01623d
Maximiliano Leiva <apumaleiva@gmail.com>
2024-11-12 01:40:50 -0300

    Variables GO - int, uint, string

--- variables/main.go
+++ variables/main.go
@@ -8,6 +8,20 @@ func main() {
 
 	var myIntVar int
 
+	myIntVar = -12
+
 	fmt.Println("My variable is: ", myIntVar)
+
+	var myUintVar uint
+
+	myUintVar = 12
+
+	fmt.Println("My variable is: ", myUintVar)
+
+	var myStringVar string
+
+	myStringVar = "Hola ke ace"
+
+	fmt.Println("My variable is: ", myStringVar)
 
 }
