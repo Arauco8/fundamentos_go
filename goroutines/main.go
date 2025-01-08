e58a72f0fbba3404510fb9724ce5e5f9113e54d0
Maximiliano Leiva <apumaleiva@gmail.com>
2025-01-08 01:30:32 -0300

    Concurrencia y paralelismo - Channels

--- goroutines/main.go
+++ goroutines/main.go
@@ -11,6 +11,25 @@ func main() {
 	time.Sleep(3 * time.Second)
 
 	//si el programa termina, las goroutines también terminan no importa si no terminaron
+
+	//channels
+
+	myFirstChannel := make(chan string) //crea un canal de tipo string con la palabra reservada chan
+	go myProcessWithChannel("C", myFirstChannel)
+
+	result := <-myFirstChannel
+	fmt.Println(result)
+	close(myFirstChannel) //cierra el canal
+}
+
+func myProcessWithChannel(p string, c chan string) {
+	i := 0
+	for i < 20 {
+		i++
+		time.Sleep(150 * time.Millisecond)
+		fmt.Printf("process: %s - num: %d\n", p, i)
+	}
+	c <- "ok" //envia el valor de p al canal, asigna un valor al canal
 }
 
 func myProcess(p string) {
