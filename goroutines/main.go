cc1f1cb646871b01c3872fbc8862755a380596d0
Maximiliano Leiva <apumaleiva@gmail.com>
2025-01-08 01:36:35 -0300

    Concurrencia y paralelismo - Channels B

--- goroutines/main.go
+++ goroutines/main.go
@@ -14,15 +14,38 @@ func main() {
 
 	//channels
 
-	myFirstChannel := make(chan string) //crea un canal de tipo string con la palabra reservada chan
-	go myProcessWithChannel("C", myFirstChannel)
+	myFirstChannel := make(chan string)          //crea un canal de tipo string con la palabra reservada chan
+	go myProcessWithChannel("C", myFirstChannel) //ejecuta la go routine con el canal,
 
-	result := <-myFirstChannel
+	result := <-myFirstChannel //espero a que el canal tenga un valor o se le asigne un valor
 	fmt.Println(result)
 	close(myFirstChannel) //cierra el canal
+
+	channelA := make(chan string)
+	channelB := make(chan string)
+
+	go myProcessWithChannel("D", channelA)
+	go myProcessOtherWithChannel("E", channelB)
+
+	resultA := <-channelA
+	resultB := <-channelB
+	fmt.Println("A: ", resultA)
+	fmt.Println("B: ", resultB)
+	close(channelA)
+	close(channelB)
 }
 
 func myProcessWithChannel(p string, c chan string) {
+	i := 0
+	for i < 20 {
+		i++
+		time.Sleep(150 * time.Millisecond)
+		fmt.Printf("process: %s - num: %d\n", p, i)
+	}
+	c <- "ok" //envia el valor de p al canal, asigna un valor al canal
+}
+
+func myProcessOtherWithChannel(p string, c chan string) {
 	i := 0
 	for i < 20 {
 		i++
