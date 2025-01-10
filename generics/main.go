01fab78594ccc28a9ff45494903b780407f076f5
Maximiliano Leiva <apumaleiva@gmail.com>
2025-01-10 01:56:45 -0300

    Generics

--- generics/main.go
+++ generics/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"cmp"
+)
 
 func main() {
 	v1 := []float64{1.3, 5.45, 12.223, 6.92, 78.102}
@@ -14,6 +18,22 @@ func main() {
 	fmt.Println()
 	anyType(1, 1)
 	anyType("1", "3")
+	anyTypeTwo(1, "5")
+	comparableType(1, 1)
+	orderedValues(2, 4)
+	fmt.Println()
+	csInt := CustomSlice[int]{1, 2, 3, 4}
+	fmt.Println(csInt)
+	csString := CustomSlice[string]{"a", "b", "c", "4"}
+	fmt.Println(csString)
+	fmt.Println()
+	x, y := Point(5), Point(2)
+	fmt.Println(MinNumber(x, y))
+	fmt.Println()
+	df := MyGenericStruct[myFirstData]{StrValue: "First", Data: myFirstData{}}
+	df.Data.PrintOne()
+	ds := MyGenericStruct[mySecondData]{StrValue: "Second", Data: mySecondData{}}
+	ds.Data.PrintTwo()
 }
 
 func sum01[N int | int32 | int64 | float32 | float64](v []N) N {
@@ -38,4 +58,52 @@ func sum02[N Number](v []N) N {
 
 func anyType[T any](v, v2 T) {
 	fmt.Println(v, v2)
+}
+
+func anyTypeTwo[T1 any, T2 any](v T1, v2 T2) {
+	fmt.Println(v, v2)
+}
+
+func comparableType[T comparable](v, v2 T) {
+	fmt.Println(v, v2)
+	fmt.Println(v != v2) // nose puede comparar con type any
+}
+
+func orderedValues[T cmp.Ordered](v, v2 T) {
+	fmt.Println(v, v2)
+	fmt.Println(v != v2)
+	fmt.Println(v < v2)
+	fmt.Println(v > v2)
+}
+
+type CustomSlice[T int | string] []T
+
+func MinNumber[T N1](v1, v2 T) T {
+	if v1 < v2 {
+		return v1
+	}
+	return v2
+}
+
+type Point int
+
+type N1 interface {
+	~int | ~int32 | ~int64 | ~float32 | ~float64 // ~ means "any type except"
+}
+
+type (
+	MyGenericStruct[T any] struct {
+		StrValue string
+		Data     T
+	}
+	myFirstData  struct{}
+	mySecondData struct{}
+)
+
+func (d myFirstData) PrintOne() {
+	fmt.Println("PrintOne")
+}
+
+func (d mySecondData) PrintTwo() {
+	fmt.Println("PrintTwo")
 }
